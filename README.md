70b93d51c726e0ce28202faeaeba696e4a2c5011
Kam Woods <kamwoods@gmail.com>
2014-02-08 23:45:36 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-dimac
-=====
+DIMAC: Disk Image Access for the Web
+------------------------------------
+<http://wiki.bitcurator.net/dimac>
 
-dimac
+# Getting started
+
+DIMAC provides simple access to disk images over the web using open source technologies including
+The Sleuth Kit, PyTSK, and Flask.
+
+To clone out the DIMAC code repo, run:
+
+* git clone https://github.com/kamwoods/dimac
+
+# Dependencies
+
+On a Debian or Ubuntu system:
+
+* sudo apt-get install python-pip
+* sudo pip install flask
+
+(More instructions on running the DIMAC Flask app coming soon...)
+
+# DIMAC Documentation, Help, and other Information
+
+Coming soon...
+[http://wiki.bitcurator.net/dimac](http://wiki.bitcurator.net/dimac).
+
+# License(s)
+
+DIMAC project documentation, and other non-software products of the DIMAC team are subject to the the Creative Commons Attribution 2.0 Generic license (CC By 2.0).
+
+Unless otherwise indicated, software items in this repository are distributed under the terms of the GNU General Public License, Version 3. See the text file "COPYING" for further details about the terms of this license.
+
+
---
+++ python/imgaccess.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import pytsk3
+
+app = Flask(__name__)
+
+@app.route("/")
+
+# Sample hello world for testing
+# def hello():
+#     return "Hello World!"
+
+def tsktest():
+    # Step 1: get an IMG_INFO object
+    img = pytsk3.Img_Info("/home/bcadmin/Desktop/jo-work-usb-2009-12-11.E01")
+
+    ## Step 2: get a Volume_Info object
+    volume = pytsk3.Volume_Info(img)
+
+    ## Step 3: Iterate over all the partitions.
+    retstr = 'PARTITIONS ON THIS DISK:' + '<br>'
+    for part in volume:
+        #print part.addr, part.desc, part.start, part.len
+        retstr += str(part.addr) + ' ' + str(part.desc) + ' ' + str(part.start) + ' ' + str(part.len) + '</br>'
+
+    retstr += '</br>' + 'Contents of the root directory:' + '</br>'
+
+    ## Now, a hack to recognize the start location. Do NOT use this
+    ## code in production. It's just a demo.
+    fs = pytsk3.FS_Info(img, offset = 63 * 512)
+
+    for directory_entry in fs.open_dir(path="/"):
+        directory_entry = directory_entry.info.name.name
+        try:
+            retstr += directory_entry.decode("utf8") + '<br>'
+            directory_entry.decode("utf8")
+        except UnicodeError:
+            pass
+    return retstr
+
+if __name__ == "__main__":
+    app.run()
