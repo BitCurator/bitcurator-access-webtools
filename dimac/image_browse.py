9be3dc25c5ad7faca67600a813cfe3256d7dcefc
Sunitha Misra <sunithanc@yahoo.com>
2014-02-18 15:10:27 -0500

    Intermediate commit

--- dimac/image_browse.py
+++ dimac/image_browse.py
@@ -6,8 +6,9 @@ from dimac import app
 
 image_list = []
 file_list_root = []
-dirDictList = []
+dirDictList = [[]]
 image_dir = "/home/bcadmin/disk_images" # HARDCODED: FIXME
+num_images = 0
 
 @app.route("/")
 
@@ -33,6 +34,9 @@ def bcBrowseImages():
   
     # Render the template for main page.
     print 'D: Image_list: ', image_list
+    global num_images
+    num_images = len(image_list)
+
     return render_template('fl_temp_ext.html', image_list=image_list, np=dm.num_partitions)
 
 def dimacGetImageIndex(image, is_path):
@@ -65,7 +69,7 @@ def image(image_name):
 # Template rendering for Directory Listing per partition 
 #
 @app.route('/image/<image_name>/<image_partition>')
-def directory_list(image_name, image_partition):
+def root_directory_list(image_name, image_partition):
     print("Files: Rendering Template with files for partition: ", 
                             image_name, image_partition)
     image_index = dimacGetImageIndex(str(image_name), False)
@@ -83,7 +87,7 @@ def directory_list(image_name, image_partition):
 #
 @app.route('/image/<image_name>/<image_partition>/<file_path>')
 def file_clicked(image_name, image_partition, file_path):
-    print("Files: Rendering Template with contents of a file: ", 
+    print("Files: Rendering Template for subdirectory or contents of a file: ", 
           image_name, image_partition, file_path)
     
     image_index = dimacGetImageIndex(str(image_name), False)
@@ -92,63 +96,84 @@ def file_clicked(image_name, image_partition, file_path):
     file_path = '/'+file_path
 
     # First find out if it is a file or a directory.
-    ##'''
-    for dir_item in dirDictList:
-        if dir_item['dirname'] == file_name:
-            print("File found in dirDictList: ", file_name)
-            
-            break
-        else:
-            continue
-    else:
-        print("File %s could be a regular file. " %file_name)
-    ##'''
+    print("D: FILE Clicked: image: {}, part:{}, file_path:{}".format(image_name, image_partition, file_path))
+
+    dm = dimac()
 
     # For the files in the root directory, current directory is /. 
     # To reuse this func for other directories below the root,
     # This needs to be set appropriately. For now, this will do: FIXME.
     current_dir = "/"
 
-    dm = dimac()
+    temp_list = file_path.split("/")
+    # FIXME: Add comments here: Can test this logic only after the
+    # partition_num issue is fixed and we can go deeper in the 
+    # directory structure.
+    if len(temp_list) == 2:
+        # Just one directory. So parent directory is '/'
+        current_dir = "/"
+    else:
+        print("D: temp comment: temp_list BEFORE: ", temp_list)
+        temp_list = temp_list[0:(len(temp_list)-1)]
+        print("D: temp comment: temp_list AFTER: ", temp_list)
+        current_dir = ''.join(temp_list)
+        print("D: temp comment: NEW Current DIR: ", current_dir)
+    
+
+    print("D: current_dir: ", current_dir)
     file_list, fs = dm.dimacGenFileList(image_path, image_index, 
                                         int(image_partition), current_dir)
 
     # Look for file_name in file_list
     for item in file_list:
+        ## print("D: item-name= {} fule_name={} ".format(item['name'], file_name))
         if item['name'] == file_name:
-            print("File Found in the list: ", file_name)
+            print("D: File Found in the list: ", file_name)
+            break
+    else:
+        print("File {} not found in file_list".format(file_name))
             
-            if item['isdir'] == True:
-                # It is a directory call the template to display file
-                # list under this directory FIXME: Add template rendering
-                # code for a directory: Can reuse the same templates used before
-                for dir_item in dirDictList:
-                    print dir_item
+    if item['isdir'] == True:
+        file_list, fs = dm.dimacGenFileList(image_path, image_index, 
+                                        int(image_partition), file_path)
+        # It is a directory call the template to display file
+        # list under this directory FIXME: Add template rendering
+        # code for a directory: Can reuse the same templates used before
+        new_dir = file_path
+        print("D: temp comment: current_dir:new_dir ", current_dir, new_dir)
+        return render_template('fl_dir_temp_ext.html', 
+                   image_name=str(image_name), 
+                   partition_num=image_partition, 
+                   new_dir=new_dir,
+                   file_list=file_list)
 
-            else:
-                f = fs.open_meta(inode=item['inode'])
-            
-                # Read data and store it in a string
-                offset = 0
-                size = f.info.meta.size
-                BUFF_SIZE = 1024 * 1024
+    else:
+        f = fs.open_meta(inode=item['inode'])
+    
+        # Read data and store it in a string
+        offset = 0
+        size = f.info.meta.size
+        BUFF_SIZE = 1024 * 1024
 
-                while offset < size:
-                    available_to_read = min(BUFF_SIZE, size - offset)
-                    data = f.read_random(offset, available_to_read)
-                    if not data: 
-                        print("Done with reading")
-                        break
+        while offset < size:
+            available_to_read = min(BUFF_SIZE, size - offset)
+            data = f.read_random(offset, available_to_read)
+            if not data: 
+                print("Done with reading")
+                break
 
-                    offset += len(data)
-                    #print data
+            offset += len(data)
+            #print data
 
-                return render_template('fl_filecat_temp_ext.html', 
-                            image_name=str(image_name), 
-                            partition_num=image_partition, 
-                            file_name=file_name,
-                            contents=str(data))
-                            #contents = data.decode("utf-8"))
+        return data
+        '''
+        return render_template('fl_filecat_temp_ext.html', 
+                    image_name=str(image_name), 
+                    partition_num=image_partition, 
+                    file_name=file_name,
+                    contents=str(data))
+                    #contents = data.decode("utf-8"))
+        '''
 class dimac:
     num_partitions = 0
     part_array = ["image_path", "addr", "slot_num", "start_offset", "desc"]
@@ -159,6 +184,7 @@ class dimac:
         img = pytsk3.Img_Info(image_path)
         volume = pytsk3.Volume_Info(img)
         self.partDictList.append([])
+
         for part in volume:
             # The slot_num field of volume object has a value of -1
             # for non-partition entries - like Unallocated partition
@@ -186,25 +212,12 @@ class dimac:
                 # start_offset.
                 fs = pytsk3.FS_Info(img, offset=(part.start * 512))
 
-                file_list_root = self.dimacListFiles(fs, "/")
-                print(file_list_root) 
+                # First level files and directories off the root
+                # Builds dirDictList (global) and returns file_list for
+                # the root directory 
+                file_list_root = self.dimacListFiles(fs, "/", image_index, part.slot_num)
+                ## print(file_list_root) 
     
-                '''
-                # Open the directory node based on the path
-                directory = fs.open_dir(path="/")
-    
-                file_list[image = []
-                num_dirs = 0
-                # Iterate over all files in the directory
-                for f in directory:
-                    if f.info.meta.type == 2:
-                        num_dirs += 1
-                        ## print "File %s is a Directory" %f.info.name.name
-                    file_list.append(f.info.name.name)
-                
-                ## print "Number of directoriues: ", num_dirs
-                print file_list
-                '''
         image_name = os.path.basename(image_path)
         self.num_partitions_ofimg[image_name] = self.num_partitions
         ## print ("D: Number of Partitions for image = ", image_name, self.num_partitions)
@@ -219,7 +232,7 @@ class dimac:
         # start_offset.
         fs = pytsk3.FS_Info(img, offset=(part_start * 512))
 
-        file_list_root = self.dimacListFiles(fs, root_path)
+        file_list_root = self.dimacListFiles(fs, root_path, image_index, partition_num)
 
         return file_list_root, fs
         
@@ -227,24 +240,20 @@ class dimac:
     dimacFileInfo = ['name', 'size', 'mode', 'inode', 'p_inode', 'mtime', 'atime', 'ctime', 'isdir']
     dimacDirInfo = ['dirname', 'inode', 'par_inode', 'dirpath']
 
-    def dimacListFiles(self, fs, path):
+    def dirInDirList(self, dirname, inode, image_index, partnum):
+        print ("Checking if dirname:inode exists in the dictlist: ",
+                     dirname, inode, image_index, partnum)
+
+    def dimacListFiles(self, fs, path, image_index, partition_num):
         file_list = []
+        print("Listing Directory for PATH: ", path)
         directory = fs.open_dir(path=path)
         i=0
-        print("Listing Directory for PATH: ", path)
         for f in directory:
             is_dir = False
-            ## print D: f.info.name.name, f.info.meta.size, f.info.meta.addr, f.info.name.meta_addr, f.info.name.par_addr, f.info.meta.mode, f.info.meta.type
+            #print f.info.name.name, f.info.meta.size, f.info.meta.addr, f.info.name.meta_addr, f.info.name.par_addr, f.info.meta.mode, f.info.meta.type
             if f.info.meta.type == 2:
                 is_dir = True
-
-                # Append the inode and parent's inode to dirDictList
-                global dirDictList
-                dirDictList.append({self.dimacDirInfo[0]:re.escape(f.info.name.name), \
-                              self.dimacDirInfo[1]:f.info.meta.addr, \
-                              self.dimacDirInfo[2]:f.info.name.par_addr, \
-                              self.dimacDirInfo[3]:path })       
-
             file_list.append({self.dimacFileInfo[0]:f.info.name.name, \
                               self.dimacFileInfo[1]:f.info.meta.size, \
                               self.dimacFileInfo[2]:f.info.meta.mode, \
@@ -256,8 +265,6 @@ class dimac:
                               self.dimacFileInfo[8]:is_dir })
 
         return file_list
-
-    
 
 if __name__ == "__main__":
     dm = dimac()
