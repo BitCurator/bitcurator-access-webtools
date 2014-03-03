b8c191fc888c96b19c781b6156d5db604eba2668
Kam Woods <kamwoods@gmail.com>
2014-03-02 23:18:21 -0500

    Updated config script

--- dimac/config.py
+++ dimac/config.py
@@ -1,2 +1,8 @@
+# This file contains items that can be configured in DIMAC.
+#
+# IMAGEDIR - the local directory containing your disk images
+# SQLALCHEMY_DATABASE_URI - the local db URI (you must configure a postgres
+#                           database before running the main script.
+
 IMAGEDIR = "/home/bcadmin/disk_images"
 SQLALCHEMY_DATABASE_URI = "postgresql://bcadmin:bcadmin@localhost/bcdb"
