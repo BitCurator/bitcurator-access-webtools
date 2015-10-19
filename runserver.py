36686c7d4e6882f505a23049ffc6ebb4e5d20cb6
Kam Woods <kamwoods@gmail.com>
2015-10-19 19:19:24 -0400

    Minor runserver update

--- runserver.py
+++ runserver.py
@@ -2,14 +2,12 @@
 # coding=UTF-8
 #
 # BitCurator Access Webtools (Disk Image Access for the Web)
-# Copyright (C) 2014
-# All rights reserved.
 #
 # This code is distributed under the terms of the GNU General Public
 # License, Version 3. See the text file "COPYING" for further details
 # about the terms of this license.
 #
-# This is a python script to run the main app server
+# This is a python script to run the main application server
 
 from bcaw import app
 #from bcaw import bcaw_db
