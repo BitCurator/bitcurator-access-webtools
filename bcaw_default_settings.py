8a5928cea6625f3e1aa05b574149a29c4b2081b5
Sunitha Misra <sunithanc@yahoo.com>
2015-12-12 02:06:16 -0500

    Indexing through Celery implementation

--- bcaw_default_settings.py
+++ bcaw_default_settings.py
@@ -14,5 +14,11 @@ FILESEARCH_DB = True
 
 # FILE_IDEXDIR is the directory where the index for filename search is stored.
 FILENAME_INDEXDIR = "/vagrant/filenames_to_index"
+
+# Celery related configuration: RabbitMQ is used as the broker and the backend. 
+# The broker URL tells Celery where the broker service is running.
+# The backend is the resource which returns the results of a completed task 
+# from Celery. amqp is a custom protocol that RabbitMQ utilizes.
+# (http://www.amqp.org/)
 CELERY_BROKER_URL = 'amqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'amqp://guest@localhost//'
