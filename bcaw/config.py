69e3b435e610fcd01b2edeca84353d95b00f5fb0
Carl Wilson <carl@openplanetsfoundation.org>
2018-01-17 10:09:21 +0000

    FEAT - Configuration enhancements - made default config the default; - debugging no longer the default; - added configurable logging level; - added test config with SQLLite for unit testing; and - added config profile for analyser.

--- bcaw/config.py
+++ bcaw/config.py
@@ -12,7 +12,9 @@
 # This file contains items that can be configured in BitCurator Access Webtools.
 #
 """Module for application config goodness."""
+import logging
 import os
+import tempfile
 
 from .const import ENV_CONF_PROFILE, ENV_CONF_FILE
 
@@ -26,14 +28,17 @@ DB_PASS = 'vagrant'
 DB_NAME = 'bca_db'
 POSTGRES_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/' + DB_NAME
 LUCENE_ROOT = '/var/www/.index'
-class BaseConfig(object):
+TEMP = tempfile.gettempdir()
+
+class BaseConfig(object):# pylint: disable-msg=R0903
     """The basic default configuration."""
     HOST = HOST
     SQLALCHEMY_DATABASE_URI = POSTGRES_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
-    LOG_FORMAT = '[%(filename)-15s:%(lineno)-5d] %(message)s'
+    DEBUG = False
+    LOG_FORMAT = '[%(asctime)-15s %(filename)-15s:%(lineno)-5d] %(message)s'
     LOG_FILE = LOG_ROOT + 'bcaw.log'
+    LOG_LEVEL = logging.INFO
     LUCENE_INDEX_DIR = LUCENE_ROOT
     GROUPS = [
         {
@@ -43,15 +48,32 @@ class BaseConfig(object):
         }
     ]
 
-
-class DevConfig(BaseConfig):
+class DevConfig(BaseConfig):# pylint: disable-msg=R0903
     """Development config extras."""
+    NAME = 'Development'
     DEBUG = True
-    LOG_FORMAT = '[%(levelname)-8s %(filename)-15s:%(lineno)-5d %(funcName)-30s] %(message)s'
+    LOG_LEVEL = logging.DEBUG
+    LOG_FORMAT = '[%(asctime)-15s %(levelname)-8s %(filename)-15s:'+\
+                 '%(lineno)-5d %(funcName)-30s] %(message)s'
+
+class TestConfig(DevConfig):# pylint: disable-msg=R0903
+    """Developer level config, with debug logging and long log format."""
+    NAME = 'Testing'
+    SQL_PATH = os.path.join(TEMP, 'test.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + SQL_PATH
+    LOG_FILE = os.path.join(TEMP, 'bcaw.log')
+
+class AnalyserConfig(BaseConfig):# pylint: disable-msg=R0903
+    """Configure Image analysis logging."""
+    NAME = "Analyser"
+    LOG_FORMAT = '[%(asctime)-15s %(levelname)-8s %(filename)-15s:'+\
+                 '%(lineno)-5d %(funcName)-30s] %(message)s'
 
 CONFIGS = {
+    "default": 'bcaw.config.BaseConfig',
     "dev": 'bcaw.config.DevConfig',
-    "default": 'bcaw.config.BaseConfig'
+    "test": 'bcaw.config.TestConfig',
+    "analyser": 'bcaw.config.AnalyserConfig'
 }
 
 def configure_app(app):
