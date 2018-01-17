3937e7a404a1a5b50b7371c2f1d100f4930ad1c5
Carl Wilson <carl@openplanetsfoundation.org>
2018-01-17 10:23:35 +0000

    FEAT - Group image handling - images can now be in multiple groups; - indexing recorded and logged; - indexing progress can be tracked on image screens; - added All Images group to demonstrate; - better logging and configuration; and - added unit test framework with dummy DB for improved testing.

---
+++ tests/const.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding=UTF-8
+#
+# BitCurator Access Webtools (Disk Image Access for the Web)
+# Copyright (C) 2014 - 2016
+# All rights reserved.
+#
+# This code is distributed under the terms of the GNU General Public
+# License, Version 3. See the text file "COPYING" for further details
+# about the terms of this license.
+#
+"""Constants used across test modules"""
+import os
+
+ENV_CONF_PROFILE = 'BCAW_CONFIG'
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_NAME = "Test Name"
+TEST_DESCRIPTION = "Test Description"
