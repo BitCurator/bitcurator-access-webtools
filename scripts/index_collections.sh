36b92d60006ae3f49968d299be03d91fea548733
Carl Wilson <carl@openplanetsfoundation.org>
2018-01-17 10:05:07 +0000

    FEAT - Improved indexing script - commented indexing script; and - added config variable for "production" logging.

--- scripts/index_collections.sh
+++ scripts/index_collections.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+# Set directory to install dir, this should be templated
 cd /var/www/bcaw
+# activate the virtual env
 source venv/bin/activate
+# Set config format to disable logging debug, comment out for full logging
+export BCAW_CONFIG='analyser'
+# Run the analyser
 python -m bcaw.image_analyser
